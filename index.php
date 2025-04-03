122faa0a650ed8126d0b8757da3266a896465bb2
MirkoDangelo06 <dangelo.mirko@itismeucci.com>
2025-04-03 12:09:46 +0200

    fixes login and button cards

--- index.php
+++ index.php
@@ -115,7 +115,7 @@
           echo '    <div class="card-body">';
           echo '      <h5 class="card-title">' . htmlspecialchars($row['nome_attivita']) . '</h5>';
           echo '      <p class="card-text">' . htmlspecialchars($row['descrizione']) . '</p>';
-          echo '<a href="details.php?id=' . $row['id_attivita'] . '" class="button_card btn"> <svg class="svgIcon" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg> Maggiori Informazioni</a>';
+          echo '<a href="details.php?id=' . $row['id_attivita'] . '" class="btn button1">Maggiori Informazioni</a>';
           echo '    </div>';
           echo '  </div>';
           echo '</div>';
@@ -127,6 +127,7 @@
     </div><!-- chiusura container -->
   <form>     
 </div><!-- chiusura del div principale -->
+
 
 
 
